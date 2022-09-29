efdff05dcd079970be9c4d15de3ef96f9febab04
Faiaz Ar Rafi <faiazarrafi@gmail.com>
2022-09-29 15:55:24 +0600

    first q/a added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -4,10 +4,12 @@ import './Blog.css'
 const Blog = () => {
     return (
         <div>
+            <h1>Have a Read</h1>
             <article className='blog-design'>
+
                 <div>
                     <h3>How does React work ?</h3>
-                    <p></p>
+                    <p>React uses a declarative paradigm that makes it easier to reason about the  application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when the data changes. The declarative view makes the code more predictable and easier to debug.</p>
                 </div>
                 <div>
                     <h3>What is the difference between Props and State ?</h3>
--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Details.css'
 
-const Details = () => {
+const Details = (props) => {
+    const { info } = props
+    // console.log(props)
+    // const [info] = props
+    const [brTime, setbrTime] = useState([])
+    console.log(brTime)
+
+    let total = 0;
+    for (const activity of info) {
+        total = total + parseFloat(activity.time);
+    }
+
+    const handleBreakTime = () => {
+        const btime =
+            localStorage.setItem('Break time',)
+    }
+
     return (
         <div>
             <div className='personal-info'>
@@ -18,20 +34,20 @@ const Details = () => {
                     <small>Age</small>
                 </div>
             </div>
-            {/* <p>{info.length}</p> */}
+            <p>{info.length}</p>
             <h2>Take a Break</h2>
             <div className="add-break">
-
-                <p className="break-time">10s</p>
-                <p className="break-time">20s</p>
-                <p className="break-time">30s</p>
-                <p className="break-time">40s</p>
-                <p className="break-time">50s</p>
+                <button onClick={handleBreakTime} value='1' className="break-time">10s</button>
+                <button onClick={handleBreakTime} className="break-time">20s</button>
+                <button onClick={handleBreakTime} className="break-time">30s</button>
+                <button onClick={handleBreakTime} className="break-time">40s</button>
+                <button onClick={handleBreakTime} className="break-time">50s</button>
             </div>
+
             <h2>Workout Details</h2>
             <div className='workout-time'>
                 <h4>Workout Time :</h4>
-                <h4>1005 seconds</h4>
+                <h4>{total} seconds</h4>
             </div>
             <div className='workout-time'>
                 <h4>Break Time :</h4>
